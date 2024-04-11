47814c5c08ac76174db59f27c5c37ca6882275fc
Mark Conroy <mark@mark.ie>
2024-04-11 17:38:08 +0100

    adds basic code for onetrust

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+let confirmButton;
+
 const allConsentItems = [];
 const legitimateInterestItems = document.querySelectorAll('.fc-preference-legitimate-interest');
 const consentItems = document.querySelectorAll('.fc-preference-consent');
-let confirmButton;
 
 if (legitimateInterestItems) {
   legitimateInterestItems.forEach(item => {
@@ -23,12 +24,50 @@ if (document.querySelector('.fc-confirm-choices')) {
   confirmButton = document.querySelector('.fc-confirm-choices');
 }
 
+// Cookiebot
+// Decline all button is present
 if (document.querySelector('#CybotCookiebotDialogBodyButtonDecline')) {
   confirmButton = document.querySelector('#CybotCookiebotDialogBodyButtonDecline');
 }
 
-if (document.querySelector('.ot-pc-refuse-all-handler')) {
-  confirmButton = document.querySelector('.ot-pc-refuse-all-handler');
+// OneTrust
+// Decline all button is present
+if (document.querySelector('#onetrust-reject-all-handler')) {
+  confirmButton = document.querySelector('#onetrust-reject-all-handler');
+}
+
+const oneTrustDialogOpener = document.querySelector('#onetrust-pc-btn-handler');
+let oneTrustsubmitButton = document.querySelector('.ot-pc-refuse-all-handler');
+
+if (submitButton) {
+  submitButton.click();
+} else {
+  oneTrustDialogOpener.click();
+  handleOneTrust();
+  // Add a second check here for the submit button
+  // Redeclare the submitButton variable
+  // Call the submitButton.click() method
+
+  // use a mutation observer to check for the submit button
+  const observer = new MutationObserver((mutationsList, observer) => {
+    for(let mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        oneTrustsubmitButton = document.querySelector('.ot-pc-refuse-all-handler');
+        if (oneTrustsubmitButton) {
+          oneTrustsubmitButton.click();
+          observer.disconnect();
+        }
+      }
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+}
+
+function handleOneTrust() {
+	const submitButton = document.querySelector('.ot-pc-refuse-all-handler');
+  console.log(submitButton);
+	submitButton.click();
 }
 
 confirmButton.click();
