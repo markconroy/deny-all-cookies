a965beb80d6c0c60dc5af598fb1350e8fcf49fb2
Mark Conroy <mark@mark.ie>
2024-06-29 13:00:41 +0100

    updates url for script

--- js/f.script.js
+++ js/f.script.js
@@ -1,7 +1,7 @@
 function handleDenyAllCookiesScript() {
   const denyAllCookiesBookmarkletScript = document.createElement("script");
   denyAllCookiesBookmarkletScript.src =
-    "https://dac.mark.ie/js/script.js";
+    "https://deny-all-cookies.mark.ie/js/script.js";
   denyAllCookiesBookmarkletScript.setAttribute("id", "deny-all-cookies");
   const denyAllCookiesBookmarkletBody = document.querySelector("body");
   denyAllCookiesBookmarkletBody.appendChild(denyAllCookiesBookmarkletScript);
